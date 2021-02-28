b0fbfe7dc5babef6d21304dbd53d8d0fad03c2de
sambath <sambathmom06@gmail.com>
2021-02-28 16:04:23 +0700

    Sdk sample project

---
+++ src/main/java/sdk/sample/coreutil/util/DateFormatUtil.java
@@ -0,0 +1,7 @@
+package sdk.sample.coreutil.util;
+
+public class DateFormatUtil {
+    public static String dateString() {
+        return "2021-02-28";
+    }
+}
---
+++ src/test/java/sdk/sample/coreutil/CoreUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package sdk.sample.coreutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
